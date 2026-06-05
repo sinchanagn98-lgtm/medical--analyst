240f1eefcd4f7c9001ba9b923342ab0ebcc0fed0
sinchana <sinchanagn98@gmail.com>
2026-06-05 22:16:26 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+def load_data():
+
+    df = pd.read_csv(
+        "dataset/Medical_Device_Failure_dataset.csv"
+    )
+
+    # Convert date column
+    if 'Purchase_Date' in df.columns:
+        df['Purchase_Date'] = pd.to_datetime(df['Purchase_Date'])
+
+    return df
